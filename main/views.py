98c870c4739c42e7efc8aaab0a62288bbde8c4be
Никита ноут <n1k1ta.pop@yandex.ru>
2025-02-03 14:37:23 +0700

    first commit

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    #return HttpResponse('111')
+    return render(request, 'main/index.html', {
+        'title': 'Ведическая астрология2'
+    })
