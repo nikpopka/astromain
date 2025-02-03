8620cd5c272d4f5a03815b950ec51049606da988
Никита <n1k1ta.pop@yandex.ru>
2025-02-03 11:34:32 +0300

    git commit -m "тестовый новый комит"

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     #return HttpResponse('111')
     return render(request, 'main/index.html', {
-        'title': 'Ведическая астрология2'
+        'title': 'Ведическая астрология'
     })
